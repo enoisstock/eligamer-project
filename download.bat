257f21bc5744f6df09a271f58f61b8b9e6413ecf
enoisstock <etecki@outlook.com>
2025-12-28 15:55:06 +0100

    Update download.bat

--- download.bat
+++ download.bat
@@ -1,5 +1,19 @@
 @echo off
-set "URL=https://raw.githubusercontent.com/enoisstock/eligamer-project/main/Loader.bat"
-powershell -Command "Invoke-WebRequest -Uri '%URL%' -OutFile 'Loader.bat'"
-start Loader.bat
-exit
+title ELIGAMER - System Installer
+cls
+
+echo [1/3] Verbinde mit GitHub Repository...
+:: Pfad zu deinem Repository
+set "RAW_URL=https://raw.githubusercontent.com/enoisstock/eligamer-project/main"
+
+echo [2/3] Lade Loader herunter...
+powershell -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest -Uri '%RAW_URL%/Loader.bat' -OutFile 'Loader.bat'"
+
+if exist "Loader.bat" (
+    echo [3/3] Starte System-Loader...
+    start Loader.bat
+    exit
+) else (
+    echo FEHLER: Die Verbindung zu GitHub ist fehlgeschlagen.
+    pause
+)
